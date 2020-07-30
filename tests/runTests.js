b00ca906ce586225810ad149b1be8001ec0f3ace
Konrad Dzwinel <konrad@duckduckgo.com>
2020-07-30 21:44:38 +0200

    Integration test (#25)
    * Add integration test that crawls 4 pages in parallel.
    
    * Make linters happy.
    
    * Add EOL so that github is happy.
    
    * Fail on purpose.
    
    * Load correct version of 'assert'
    
    * catch and retrhow test failure
    
    * Ops, not try{}catch{} but .catch()
    
    * another attempt
    
    * Add a test runner.
    
    * Fix typo.
    
    * Add emoji
    
    * Catch errors in all test files and exit 1.
    
    * Small fix.
    
    * Unbreak tests.
    
    * Test should thow on unhandled rejections.
    
    * minimum node 10.17 is required to run tests
    
    * drop catching - we don't need it anymore
    
    * Improve emoji

---
+++ tests/runTests.js
@@ -0,0 +1,41 @@
+/* eslint-disable no-console */
+const path = require('path');
+const fs = require('fs');
+const {execSync} = require('child_process');
+
+/**
+ * @param {string} startPath 
+ */
+function fromDir(startPath) {
+    const files = fs.readdirSync(startPath);
+    const tests = [];
+
+    for (let i = 0; i < files.length; i++) {
+        const filename = path.join(startPath, files[i]);
+        const stat = fs.lstatSync(filename);
+        if (stat.isDirectory()) {
+            fromDir(filename).forEach(t => tests.push(t));
+        } else if (filename.indexOf('.test.js') >= 0) {
+            tests.push(filename);
+        }
+    }
+
+    return tests;
+}
+
+const tests = fromDir('./');
+
+for (const testPath of tests) {
+    console.log(`\n Running "${testPath}"…`);
+
+    try {
+        console.time(`⏱ "${testPath}"`);
+        execSync(`node --unhandled-rejections=strict ${testPath}`);
+        console.log(`✅ "${testPath}" passed`);
+    } catch (e) {
+        console.log(`🛑 "${testPath}" failed`);
+        process.exit(1);
+    } finally {
+        console.timeEnd(`⏱ "${testPath}"`);
+    }
+}
