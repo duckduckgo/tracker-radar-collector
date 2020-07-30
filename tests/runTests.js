1032de092078dbf9304216df0bb5688eaea78827
Konrad Dzwinel <kdzwinel@gmail.com>
2020-07-30 14:54:34 +0200

    Add a test runner.

---
+++ tests/runTests.js
@@ -0,0 +1,40 @@
+/* eslint-disable no-console */
+const path = require('path');
+const fs = require('fs');
+const {execSync} = require('child_process');
+
+/**
+ * @param {string} startPath 
+ */
+function fromDir(startPath) {
+    const files = fs.readdirSync(startPath);
+    const tests = [];
+
+    for (let i = 0; i < files.length; i++) {
+        const filename = path.join(startPath, files[i]);
+        const stat = fs.lstatSync(filename);
+        if (stat.isDirectory()) {
+            fromDir(filename).forEach(t => tests.push(t));
+        } else if (filename.indexOf('.test.js') >= 0) {
+            tests.push(filename);
+        }
+    }
+
+    return tests;
+}
+
+const tests = fromDir('./');
+
+for (const testPath of tests) {
+    console.log(`Running "${testPath}"…`);
+
+    try {
+        console.time(testPath);
+        execSync(`node ${testPath}`);
+    } catch (e) {
+        // console.error(out);
+        process.exit(1);
+    } finally {
+        console.timeEnd(testPath);
+    }
+}
