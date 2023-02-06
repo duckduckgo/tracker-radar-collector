2d361850820d569b49984049121fb3011c751e4f
Maxim Tsoy <maks.tsoy@gmail.com>
2023-02-06 12:04:24 +0000

    Set a timeout on browser cleanup

--- crawler.js
+++ crawler.js
@@ -321,8 +321,14 @@ module.exports = async (url, options) => {
     } finally {
         // only close the browser if it was created here and not debugging
         if (browser && !VISUAL_DEBUG) {
-            await context.close();
-            await browser.close();
+            await wait((async () => {
+                await context.close();
+                await browser.close();
+            })(), 10000).catch(e => {
+                const msg = `${chalk.red('Could not clean up the browser')} ${chalk.gray(e.message)}`;
+                console.trace(msg);
+                log(msg);
+            });
         }
     }
 
