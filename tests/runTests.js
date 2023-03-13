94a1376c4e97579f99ddfd55980b5baa51a5dd63
Maxim Tsoy <maks.tsoy@gmail.com>
2023-03-13 12:13:52 +0100

    Add retries to CI tests

--- tests/runTests.js
+++ tests/runTests.js
@@ -1,10 +1,11 @@
-/* eslint-disable no-console */
+/* eslint-disable no-console, no-process-env */
 const path = require('path');
 const fs = require('fs');
 const {execSync} = require('child_process');
+const MAX_ATTEMPTS = process.env.CI ? 3 : 1;
 
 /**
- * @param {string} startPath 
+ * @param {string} startPath
  */
 function fromDir(startPath) {
     const files = fs.readdirSync(startPath);
@@ -27,15 +28,22 @@ const tests = fromDir('./');
 
 for (const testPath of tests) {
     console.log(`\nRunning "${testPath}"…`);
-
-    try {
-        console.time(`⏱ "${testPath}"`);
-        execSync(`node --unhandled-rejections=strict ${testPath}`);
-        console.log(`✅ "${testPath}" passed`);
-    } catch (e) {
-        console.log(`🛑 "${testPath}" failed`);
-        process.exit(1);
-    } finally {
-        console.timeEnd(`⏱ "${testPath}"`);
+    let attempt = 1;
+    while (attempt <= MAX_ATTEMPTS) {
+        try {
+            console.time(`⏱ "${testPath}"`);
+            execSync(`node --unhandled-rejections=strict ${testPath}`);
+            console.log(`✅ "${testPath}" passed`);
+            break;
+        } catch (e) {
+            console.log(`🛑 "${testPath}" failed, attempt ${attempt} / ${MAX_ATTEMPTS}`);
+            if (attempt >= MAX_ATTEMPTS) {
+                process.exit(1);
+            } else {
+                attempt++;
+            }
+        } finally {
+            console.timeEnd(`⏱ "${testPath}"`);
+        }
     }
 }
