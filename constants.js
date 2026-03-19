62c1054c43b1e27b465b51a725e2249a21b9bcfd
de1R0 <roi.sanchez@networks.imdea.org>
2026-03-19 10:20:20 +0100

    [ FIX ] Fix python wrapper

--- constants.js
+++ constants.js
@@ -1,7 +1,6 @@
 const path = require('path');
 
-const DEFAULT_USER_AGENT =
-    'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/135.0.0.0 Safari/537.36';
+const DEFAULT_USER_AGENT = 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/135.0.0.0 Safari/537.36';
 const MOBILE_USER_AGENT = 'Mozilla/5.0 (Linux; Android 10; K) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/135.0.0.0 Mobile Safari/537.36';
 
 /** @type {import('devtools-protocol/types/protocol').Protocol.Emulation.SetDeviceMetricsOverrideRequest} */
@@ -9,7 +8,7 @@ const DEFAULT_VIEWPORT = {
     width: 1440,
     height: 812,
     deviceScaleFactor: 0,
-    mobile: false,
+    mobile: false
 };
 
 /** @type {import('devtools-protocol/types/protocol').Protocol.Emulation.SetDeviceMetricsOverrideRequest} */
@@ -17,7 +16,7 @@ const MOBILE_VIEWPORT = {
     width: 412,
     height: 691,
     deviceScaleFactor: 2,
-    mobile: true,
+    mobile: true
 };
 
 const VISUAL_DEBUG = false;
@@ -30,4 +29,5 @@ module.exports = {
     MOBILE_VIEWPORT,
     VISUAL_DEBUG,
     CHROMIUM_DOWNLOAD_DIR,
-};
+        };
+        
